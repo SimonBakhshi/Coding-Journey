97d727abb3755ca324114d804c62f19be29270ce
Simon Bakhshi <simonbakhshi01@gmail.com>
2021-08-19 21:30:03 +0200

    Debugging script.py

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ print("   1. Venus   2. Mars    3. Jupiter")
 print("   4. Saturn  5. Uranus  6. Neptune\n")
  
 weight = 185
-planet = 6
+planet = 3
 
 # Write an if statement below:
 if planet == 1:
@@ -20,4 +20,4 @@ elif planet == 5:
 elif planet == 6:
   weight = weight * 1.19
 
-  print('Your weight', weight)
+print('Your weight', weight)
