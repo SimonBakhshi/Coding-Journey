ec83f71eb418f7159519b8a2081cd4c660e5e435
SimonBakhshi <simonbakhshi01@gmail.com>
2021-08-19 20:20:57 +0200

    Adding some functions which calculates F to C and C to F, mass multiplied by acceleration, amount of energy contained in a bomb and amount of energy a train does in 100 meters, in joules of energy.

--- Functions.py
+++ Functions.py
@@ -63,3 +63,49 @@ def estimated_time_rounded(estimated_time):
 trip_planner_welcome("Simon ")
 estimate = estimated_time_rounded(10.77)
 destination_setup(" Silkeborg ", "København ", estimate, "Car")
+
+# Variables 
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
+
+# Creating Fahrenheit to celsius and celsius to fahrenheit converteres: 
+def f_to_c(f_temp):
+  c_temp = (f_temp -32) * 5/9
+  return c_temp
+
+f100_in_celsius = f_to_c(100)
+print(f100_in_celsius)
+
+def c_to_f(c_temp):
+  f_temp = c_temp *(9/5) + 32
+  return f_temp
+
+c0_in_fahrenheit = c_to_f(0)
+print(c0_in_fahrenheit)
+
+# Creating a function which multiplies mass and acceleration:
+def get_force(mass, acceleration):
+  mass_times_acceleration = mass * acceleration
+  return mass_times_acceleration
+
+train_force = get_force(train_mass, train_acceleration)
+print("The GE train supplies ", train_force, " Newtons of force")
+
+# Creating a function which calculates the energy contained in a bomb in joules of energy:
+def get_energy(mass, c=3*10**8):
+  mass_times_c2 = mass * c**2
+  return mass_times_c2
+
+bomb_energy = get_energy(bomb_mass)
+print("A 1kg bomb supplies", bomb_energy, "Joules.")
+
+# Creating a function which calculates the amount of work the GE train does in 100 meters. The work is calculated in joules of energy.  
+def get_work(mass, acceleration, distance):
+  force_multiplied_by_distance = get_force(mass, acceleration) * distance
+  return force_multiplied_by_distance
+
+train_work = get_work(train_mass, train_acceleration, train_distance)
+print("The GE train does", train_work, "Joules of work over", train_distance, "meters.")
+
