46ee70a732bc92fea39cf2701751048a02736c6d
SimonBakhshi <simonbakhshi01@gmail.com>
2021-08-20 20:24:21 +0200

    Adding more functions to functions.py

--- Functions.py
+++ Functions.py
@@ -129,4 +129,35 @@ def over_budget(budget, food_bill, electricity_bill, internet_bill, rent):
 print(over_budget(100, 20, 30, 10, 40))
 print(over_budget(80, 20, 30, 10, 30))
 
+# Writing a program which calculates whether a number is twice as large as another:
+def twice_as_large(num1, num2):
+  if num1 > num2 * 2:
+    return True
+  else:
+    return False
+
+print(twice_as_large(10, 5))
+print(twice_as_large(11, 5))
+
+# Writing a function which checks whether the input number is divisible by ten:
+def divisible_by_ten(num):
+  if num % 10 == 0:
+    return True
+  else:
+    return False
+
+print(divisible_by_ten(20))
+print(divisible_by_ten(25))
+
+# Creating a program which checks whether two numbers added together equals ten or not. Returns the calculation i booleans. 
+def not_sum_to_ten(num1, num2):
+  if num1 + num2 != 10:
+    return True
+  else:
+    return False
+
+print(not_sum_to_ten(9,-1))
+print(not_sum_to_ten(9, 1))
+print(not_sum_to_ten(5,5))
+
 
