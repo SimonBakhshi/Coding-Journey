b4a5d85235ca7ff382552708ad9b73f32490cdd5
Simon Bakhshi <simonbakhshi01@gmail.com>
2021-09-07 18:03:29 +0200

    Adding functionality to Tictactoe.py.

--- Tictactoe.py
+++ Tictactoe.py
@@ -2,8 +2,12 @@ game = [[0,0,0],
         [0,0,0],
         [0,0,0]]
 
-count = 0 
-for row in game:
-    print(count, row)
-    count += 1
-     
+def game_board(game_map, player=0, row=0, column=0, just_display=False):
+    print("   1  2  3")
+    if not just_display:
+        game_map[row][column] = player
+    for count, row in enumerate(game_map):
+        print(count, row)
+    return game_map
+
+game = game_board(game, just_display=True)
