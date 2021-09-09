70282170276ab1e79377d8aa6ca32b4eee551f5a
Simon Bakhshi <simonbakhshi01@gmail.com>
2021-09-09 14:35:01 +0200

    Adding complete win function to Tictactoe.py.

--- Tictactoe.py
+++ Tictactoe.py
@@ -1,8 +1,8 @@
 game = [[1,0,0],
-        [1,1,1],
-        [1,0,2]]
+        [1,1,0],
+        [2,0,1]]
 
-'''def game_board(game_map, player=0, row=0, column=0, just_display=False):
+def game_board(game_map, player=0, row=0, column=0, just_display=False):
     try:
         print("   0  1  2")
         if not just_display:
@@ -15,24 +15,36 @@ game = [[1,0,0],
     except Exception as e:
         print("Something went very wrong!", e)
 
-game = game_board(game, just_display=True)
-game = game_board(game, player=1, row=2, column=2)'''
 
-
-
-# Horizontal win:
-'''def win(current_game):
+def win(current_game):
+    # Horizontal win:
     for row in game:
         print(row)
         if row.count(row[0]) == len(row) and row[0] != 0:
-            print("Winner!!!")
+            print(f"player{row[0]} is the winner horizontally (—)!")
 
-win(game)'''
+    # Diagonal win:
+    diags = []
+    for col, row in enumerate(reversed(range(len(game)))):
+        diags.append(game[row][col])
+    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+        print(f"player{diags[0]} is the winner diagonally (/)!")
 
-# Vertical win:
-for col in range(len(game)):
-    check = []
-    for row in game:
-        check.append(row[col])
-    if check.count(row[0]) == len(check) and check[0] != 0:
-        print("Winner!!!")
+    diags = []
+    for ix in range(len(game)):
+        diags.append(game[ix][ix])
+    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+        print(f"player{diags[0]} is the winner horizontally (\\)!")
+
+    # Vertical win:
+    for col in range(len(game)):
+        check = []
+        for row in game:
+            check.append(row[col])
+        if check.count(check[0]) == len(check) and check[0] != 0:
+            print(f"player{check[0]} is the winner vertically (│)!")
+
+win(game)
+
+'''game = game_board(game, just_display=True)
+game = game_board(game, player=1, row=2, column=2)''' 
