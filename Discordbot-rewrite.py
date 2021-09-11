ae1d9e093c75fba81ac6e0c88483fbf473f0f45b
Simon Bakhshi <simonbakhshi01@gmail.com>
2021-09-12 01:31:08 +0200

    Adding Magic 8ball functionality to Discordbot.

--- Discordbot-rewrite.py
+++ Discordbot-rewrite.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands 
 
 client = commands.Bot(command_prefix = ".")
@@ -8,8 +9,33 @@ async def on_ready():
     print("The bot is online.")
 
 @client.command()
-async def joey(ctx):
-    await ctx.send(f"moe {client.latency}")
+async def ping(ctx):
+    await ctx.send(f"Your ping is: {round(client.latency * 1000)}ms")
+
+@client.command(aliases=["8ball", "test"])
+async def _8ball(ctx, *, question):
+    responses = [ "It is certain.",
+                "It is decidedly so.",
+                "Without a doubt.",
+                "Yes definitely.",
+                "You may rely on it.",
+                "As I see it, yes.",
+                "Most likely.",
+                "Outlook good.",
+                "Yes.",
+                "Signs point to yes.",
+                "Reply hazy, try again.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again.",
+                "Don't count on it.",
+                "My reply is no.",
+                "My sources say no.",
+                "Outlook not so good.",
+                "Very doubtful."]
+    await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
+
 
 @client.event
 async def on_member_join(member):
@@ -20,4 +46,4 @@ async def on_member_remove(member):
     print(f"{member} has left a server")
 
 
-client.run("xxxxxxxxxxx")
+client.run("xxxxxxxxxxxxxxxxx")
