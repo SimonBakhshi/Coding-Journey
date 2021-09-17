1a1eb1ec4c51ad02e7c859203d75bcddae1b63a1
Simon Bakhshi <simonbakhshi01@gmail.com>
2021-09-17 12:57:47 +0200

    Updating Scrabble.py functionality.

--- Scrabble.py
+++ Scrabble.py
@@ -1,7 +1,9 @@
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
-
 letters_to_points = {key:values for key, values in zip(letters, points)}
+
+letters += [letter.lower() for letter in letters] 
+points *= 2
 
 letters_to_points[" "] = 0
 #print(letters_to_points)
@@ -24,9 +26,20 @@ player_to_words = {
 #print(player_to_words)
 
 player_to_points = {}
-for player, words in player_to_words.items():
-  player_points = 0
-  for word in words:
-    player_points += score_word(word)
-  player_to_points[player] = player_points
+
+def update_point_totals():
+  for player, words in player_to_words.items():
+    player_points = 0
+    for word in words:
+      player_points += score_word(word)
+    player_to_points[player] = player_points
+
+update_point_totals()
 print(player_to_points)
+
+def play_word(player, word):
+  player_to_words[player].append(word)
+  update_point_totals()
+
+play_word("player1", "MOWGLI")
+print(player_to_words)
